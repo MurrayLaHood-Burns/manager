ac6e843dce8b1a2939948177e82d75cfbfd5243f
MurrayLaHood-Burns <murray.lahoodburns@gmail.com>
2017-08-31 10:03:57 -0600

    finish lecture 138

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,5 @@
+export const EMPLOYEE_UPDATE = 'employee_update';
+
 export const LOGIN_USER = 'login_user';
 export const LOGIN_USER_FAIL = 'login_user_fail';
 export const LOGIN_USER_SUCCESS = 'login_user_success';
